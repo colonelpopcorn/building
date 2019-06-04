d315d8c26c83643b67aa3ff90999a1c78639c862
Jonathan Ling <jonathan.ling@hoteleffectiveness.com>
2019-06-04 15:28:45 -0400

    Start a Java project.

---
+++ src/main/java/shamrocksweb/Building.java
@@ -0,0 +1,13 @@
+package shamrocksweb;
+
+/**
+ * Hello world!
+ *
+ */
+public class Building
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/shamrocksweb/BuildingTest.java
@@ -0,0 +1,20 @@
+package shamrocksweb;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple Building.
+ */
+public class BuildingTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
